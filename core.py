b29ad4207b3186b6a756401b0e9e9c23e231d272
adarsh342 <126852443+adarsh342@users.noreply.github.com>
2024-08-30 22:40:20 +0530

    Update core.py

--- core.py
+++ core.py
@@ -15,12 +15,12 @@ import shutil
 import argparse
 import onnxruntime
 import tensorflow
-import roop.globals
-import roop.metadata
-import roop.ui as ui
-from roop.predictor import predict_image, predict_video
-from roop.processors.frame.core import get_frame_processors_modules
-from roop.utilities import has_image_extension, is_image, is_video, detect_fps, create_video, extract_frames, get_temp_frame_paths, restore_audio, create_temp, move_temp, clean_temp, normalize_output_path
+import swapper.globals
+import swapper.metadata
+import swapper.ui as ui
+from swapper.predictor import predict_image, predict_video
+from swapper.processors.frame.core import get_frame_processors_modules
+from swapper.utilities import has_image_extension, is_image, is_video, detect_fps, create_video, extract_frames, get_temp_frame_paths, restore_audio, create_temp, move_temp, clean_temp, normalize_output_path
 
 warnings.filterwarnings('ignore', category=FutureWarning, module='insightface')
 warnings.filterwarnings('ignore', category=UserWarning, module='torchvision')
@@ -47,29 +47,29 @@ def parse_args() -> None:
     program.add_argument('--max-memory', help='maximum amount of RAM in GB', dest='max_memory', type=int)
     program.add_argument('--execution-provider', help='available execution provider (choices: cpu, ...)', dest='execution_provider', default=['cpu'], choices=suggest_execution_providers(), nargs='+')
     program.add_argument('--execution-threads', help='number of execution threads', dest='execution_threads', type=int, default=suggest_execution_threads())
-    program.add_argument('-v', '--version', action='version', version=f'{roop.metadata.name} {roop.metadata.version}')
+    program.add_argument('-v', '--version', action='version', version=f'{swapper.metadata.name} {swapper.metadata.version}')
 
     args = program.parse_args()
 
-    roop.globals.source_path = args.source_path
-    roop.globals.target_path = args.target_path
-    roop.globals.output_path = normalize_output_path(roop.globals.source_path, roop.globals.target_path, args.output_path)
-    roop.globals.headless = roop.globals.source_path is not None and roop.globals.target_path is not None and roop.globals.output_path is not None
-    roop.globals.frame_processors = args.frame_processor
-    roop.globals.keep_fps = args.keep_fps
-    roop.globals.keep_frames = args.keep_frames
-    roop.globals.skip_audio = args.skip_audio
-    roop.globals.many_faces = args.many_faces
-    roop.globals.reference_face_position = args.reference_face_position
-    roop.globals.reference_frame_number = args.reference_frame_number
-    roop.globals.similar_face_distance = args.similar_face_distance
-    roop.globals.temp_frame_format = args.temp_frame_format
-    roop.globals.temp_frame_quality = args.temp_frame_quality
-    roop.globals.output_video_encoder = args.output_video_encoder
-    roop.globals.output_video_quality = args.output_video_quality
-    roop.globals.max_memory = args.max_memory
-    roop.globals.execution_providers = decode_execution_providers(args.execution_provider)
-    roop.globals.execution_threads = args.execution_threads
+    swapper.globals.source_path = args.source_path
+    swapper.globals.target_path = args.target_path
+    swapper.globals.output_path = normalize_output_path(swapper.globals.source_path, swapper.globals.target_path, args.output_path)
+    swapper.globals.headless = swapper.globals.source_path is not None and swapper.globals.target_path is not None and swapper.globals.output_path is not None
+    swapper.globals.frame_processors = args.frame_processor
+    swapper.globals.keep_fps = args.keep_fps
+    swapper.globals.keep_frames = args.keep_frames
+    swapper.globals.skip_audio = args.skip_audio
+    swapper.globals.many_faces = args.many_faces
+    swapper.globals.reference_face_position = args.reference_face_position
+    swapper.globals.reference_frame_number = args.reference_frame_number
+    swapper.globals.similar_face_distance = args.similar_face_distance
+    swapper.globals.temp_frame_format = args.temp_frame_format
+    swapper.globals.temp_frame_quality = args.temp_frame_quality
+    swapper.globals.output_video_encoder = args.output_video_encoder
+    swapper.globals.output_video_quality = args.output_video_quality
+    swapper.globals.max_memory = args.max_memory
+    swapper.globals.execution_providers = decode_execution_providers(args.execution_provider)
+    swapper.globals.execution_threads = args.execution_threads
 
 
 def encode_execution_providers(execution_providers: List[str]) -> List[str]:
@@ -99,10 +99,10 @@ def limit_resources() -> None:
             tensorflow.config.experimental.VirtualDeviceConfiguration(memory_limit=1024)
         ])
     # limit memory usage
-    if roop.globals.max_memory:
-        memory = roop.globals.max_memory * 1024 ** 3
+    if swapper.globals.max_memory:
+        memory = swapper.globals.max_memory * 1024 ** 3
         if platform.system().lower() == 'darwin':
-            memory = roop.globals.max_memory * 1024 ** 6
+            memory = swapper.globals.max_memory * 1024 ** 6
         if platform.system().lower() == 'windows':
             import ctypes
             kernel32 = ctypes.windll.kernel32  # type: ignore[attr-defined]
@@ -122,86 +122,86 @@ def pre_check() -> bool:
     return True
 
 
-def update_status(message: str, scope: str = 'ROOP.CORE') -> None:
+def update_status(message: str, scope: str = 'SWAPPER.CORE') -> None:
     print(f'[{scope}] {message}')
-    if not roop.globals.headless:
+    if not swapper.globals.headless:
         ui.update_status(message)
 
 
 def start() -> None:
-    for frame_processor in get_frame_processors_modules(roop.globals.frame_processors):
+    for frame_processor in get_frame_processors_modules(swapper.globals.frame_processors):
         if not frame_processor.pre_start():
             return
     # process image to image
-    if has_image_extension(roop.globals.target_path):
-        if predict_image(roop.globals.target_path):
+    if has_image_extension(swapper.globals.target_path):
+        if predict_image(swapper.globals.target_path):
             destroy()
-        shutil.copy2(roop.globals.target_path, roop.globals.output_path)
+        shutil.copy2(swapper.globals.target_path, swapper.globals.output_path)
         # process frame
-        for frame_processor in get_frame_processors_modules(roop.globals.frame_processors):
+        for frame_processor in get_frame_processors_modules(swapper.globals.frame_processors):
             update_status('Progressing...', frame_processor.NAME)
-            frame_processor.process_image(roop.globals.source_path, roop.globals.output_path, roop.globals.output_path)
+            frame_processor.process_image(swapper.globals.source_path, swapper.globals.output_path, swapper.globals.output_path)
             frame_processor.post_process()
         # validate image
-        if is_image(roop.globals.target_path):
+        if is_image(swapper.globals.target_path):
             update_status('Processing to image succeed!')
         else:
             update_status('Processing to image failed!')
         return
     # process image to videos
-    if predict_video(roop.globals.target_path):
+    if predict_video(swapper.globals.target_path):
         destroy()
     update_status('Creating temporary resources...')
-    create_temp(roop.globals.target_path)
+    create_temp(swapper.globals.target_path)
     # extract frames
-    if roop.globals.keep_fps:
-        fps = detect_fps(roop.globals.target_path)
+    if swapper.globals.keep_fps:
+        fps = detect_fps(swapper.globals.target_path)
         update_status(f'Extracting frames with {fps} FPS...')
-        extract_frames(roop.globals.target_path, fps)
+        extract_frames(swapper.globals.target_path, fps)
     else:
         update_status('Extracting frames with 30 FPS...')
-        extract_frames(roop.globals.target_path)
+        extract_frames(swapper.globals.target_path)
     # process frame
-    temp_frame_paths = get_temp_frame_paths(roop.globals.target_path)
+    temp_frame_paths = get_temp_frame_paths(swapper.globals.target_path)
     if temp_frame_paths:
-        for frame_processor in get_frame_processors_modules(roop.globals.frame_processors):
+        for frame_processor in get_frame_processors_modules(swapper.globals.frame_processors):
             update_status('Progressing...', frame_processor.NAME)
-            frame_processor.process_video(roop.globals.source_path, temp_frame_paths)
+            frame_processor.process_video(swapper.globals.source_path, temp_frame_paths)
             frame_processor.post_process()
     else:
         update_status('Frames not found...')
         return
     # create video
-    if roop.globals.keep_fps:
-        fps = detect_fps(roop.globals.target_path)
+    if swapper.globals.keep_fps:
+        fps = detect_fps(swapper.globals.target_path)
         update_status(f'Creating video with {fps} FPS...')
-        create_video(roop.globals.target_path, fps)
+        create_video(swapper.globals.target_path, fps)
     else:
         update_status('Creating video with 30 FPS...')
-        create_video(roop.globals.target_path)
+        create_video(swapper.globals.target_path)
     # handle audio
-    if roop.globals.skip_audio:
-        move_temp(roop.globals.target_path, roop.globals.output_path)
+    if swapper.globals.skip_audio:
+        move_temp(swapper.globals.target_path, swapper.globals.output_path)
         update_status('Skipping audio...')
     else:
-        if roop.globals.keep_fps:
+        if swapper.globals.keep_fps:
             update_status('Restoring audio...')
         else:
             update_status('Restoring audio might cause issues as fps are not kept...')
-        restore_audio(roop.globals.target_path, roop.globals.output_path)
+        restore_audio(swapper.globals.target_path, swapper.globals.output_path)
     # clean temp
     update_status('Cleaning temporary resources...')
-    clean_temp(roop.globals.target_path)
+    clean_temp(swapper.globals.target_path)
     # validate video
-    if is_video(roop.globals.target_path):
+    if is_video(swapper.globals.target_path):
         update_status('Processing to video succeed!')
     else:
         update_status('Processing to video failed!')
 
 
 def destroy() -> None:
-    if roop.globals.target_path:
-        clean_temp(roop.globals.target_path)
+    if swapper.globals.target_path:
+        clean_temp(swapper.globals.target_path)
     sys.exit()
 
 
@@ -209,11 +209,11 @@ def run() -> None:
     parse_args()
     if not pre_check():
         return
-    for frame_processor in get_frame_processors_modules(roop.globals.frame_processors):
+    for frame_processor in get_frame_processors_modules(swapper.globals.frame_processors):
         if not frame_processor.pre_check():
             return
     limit_resources()
-    if roop.globals.headless:
+    if swapper.globals.headless:
         start()
     else:
         window = ui.init(start, destroy)
